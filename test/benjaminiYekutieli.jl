502eb1d9d3080351ff65515fdc3a16064d715a48
Dirk Schumacher <mail@dirk-schumacher.net>
2014-06-08 00:55:33 +0200

    Fix another test issue

--- test/benjaminiYekutieli.jl
+++ test/benjaminiYekutieli.jl
@@ -4,7 +4,7 @@ using PValueAdjust
 newPvalues = benjaminiYekutieli([0.05, 0.03, 0.01, 0.5])
 @test_approx_eq newPvalues [0.13888888888889, 0.125, 0.083333333333, 1]
 
-@test all(benjaminiYekutieli([1,1,1]) .<= 1)
+@test all(benjaminiYekutieli([1.0 ,1.0, 1.0]) .<= 1.0)
 
 @test_throws DomainError benjaminiYekutieli([-1])
 @test_throws DomainError benjaminiYekutieli([2])
