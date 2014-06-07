304ebb75386e4c1e9357e066a5647432a3a4e911
Dirk Schumacher <mail@dirk-schumacher.net>
2014-06-08 00:27:38 +0200

    Enable tests for travis

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+tests = [
+    "bonferroni",
+    "holm",
+    "benjaminiHochberg",
+    "bejaminiYekutieli"]
+
+println("Running tests:")
+
+for t in tests
+    test_fn = "$t.jl"
+    println(" * $test_fn")
+    include(test_fn)
+end
