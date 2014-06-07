619cafe1a82922920907d1146235557c3885c042
Dirk Schumacher <mail@dirk-schumacher.net>
2014-06-08 00:44:57 +0200

    Improve and fix some tests

--- test/benjaminiHochberg.jl
+++ test/benjaminiHochberg.jl
@@ -2,11 +2,11 @@ using Base.Test
 using PValueAdjust
 
 newPvalues = benjaminiHochberg([0.05, 0.03, 0.01, 0.5])
-@test_approx_eq newPvalues [0.0666, 0.06, 0.04, 0.5]
+@test_approx_eq newPvalues [0.06666666667, 0.06, 0.04, 0.5]
 
 @test all(benjaminiHochberg([1,1,1]) .<= 1)
 
-@test_throws DomainError, benjaminiHochberg([-1])
-@test_throws DomainError, benjaminiHochberg([2])
+@test_throws DomainError benjaminiHochberg([-1])
+@test_throws DomainError benjaminiHochberg([2])
 
 @test benjaminiHochberg([0.5]) == [0.5]
