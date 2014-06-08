401c48f7437b5b8959ba864f0fad1e83791b398d
Dirk Schumacher <mail@dirk-schumacher.net>
2014-06-08 13:08:03 +0200

    Fix tests

--- test/benjaminiYekutieli.jl
+++ test/benjaminiYekutieli.jl
@@ -5,7 +5,7 @@ newPvalues = benjaminiYekutieli([0.05, 0.03, 0.01, 0.5])
 @test_approx_eq newPvalues [0.13888888888889, 0.125, 0.083333333333, 1]
 
 @test_approx_eq benjaminiYekutieli([1, 0.5, 0.1, 0.001, 0.0001, 0.002]) [1.00000, 1.00000, 0.36750, 0.00735, 0.00147, 0.0098]
-@test_approc_eq benjaminiYekutieli([0.05, 0.05, 0.05]) [0.0916666666667, 0.0916666666667, 0.0916666666667]
+@test_approx_eq benjaminiYekutieli([0.05, 0.05, 0.05]) [0.0916666666667, 0.0916666666667, 0.0916666666667]
 @test all(benjaminiYekutieli([1.0 ,1.0, 1.0]) .<= 1.0)
 
 @test_throws DomainError benjaminiYekutieli([-1])
