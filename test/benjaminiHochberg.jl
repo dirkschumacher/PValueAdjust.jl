e354e8e66a673f25bffa96be7e3f453732e0a67c
Dirk Schumacher <mail@dirk-schumacher.net>
2014-06-13 17:34:34 +0200

    Fix tests

--- test/benjaminiHochberg.jl
+++ test/benjaminiHochberg.jl
@@ -1,15 +1,15 @@
 using Base.Test
 using PValueAdjust
 
-newPvalues = benjaminiHochberg([0.05, 0.03, 0.01, 0.5])
+newPvalues = padjust([0.05, 0.03, 0.01, 0.5], method = :benjaminiHochberg)
 @test_approx_eq newPvalues [0.06666666667, 0.06, 0.04, 0.5]
 
-@test_approx_eq benjaminiHochberg([1, 0.5, 0.1, 0.001, 0.0001, 0.002]) [1.0, 0.6, 0.15, 0.003, 0.0006, 0.004]
-@test_approx_eq benjaminiHochberg([0.05, 0.05, 0.05]) [0.05, 0.05, 0.05]
+@test_approx_eq padjust([1, 0.5, 0.1, 0.001, 0.0001, 0.002], method = :benjaminiHochberg) [1.0, 0.6, 0.15, 0.003, 0.0006, 0.004]
+@test_approx_eq padjust([0.05, 0.05, 0.05], method = :benjaminiHochberg) [0.05, 0.05, 0.05]
 
-@test all(benjaminiHochberg([1,1,1]) .<= 1)
+@test all(padjust([1,1,1], method = :benjaminiHochberg) .<= 1)
 
-@test_throws benjaminiHochberg([-1])
-@test_throws benjaminiHochberg([2])
+@test_throws padjust([-1], method = :benjaminiHochberg)
+@test_throws padjust([2], method = :benjaminiHochberg)
 
-@test benjaminiHochberg([0.5]) == [0.5]
+@test padjust([0.5], method = :benjaminiHochberg) == [0.5]
--- test/hommel.jl
+++ test/hommel.jl
@@ -1,20 +1,20 @@
 using Base.Test
 using PValueAdjust
 
-newPvalues = hommel([0.05, 0.03, 0.01, 0.5])
+newPvalues = padjust([0.05, 0.03, 0.01, 0.5], method = :hommel)
 @test_approx_eq newPvalues [0.1, 0.075, 0.04, 0.5]
 
-@test_approx_eq hommel([1, 0.5, 0.1, 0.001, 0.0001, 0.002]) [1e+00, 1e+00, 3e-01, 5e-03, 6e-04, 8e-03]
-@test_approx_eq hommel([0.05, 0.05, 0.05]) [0.05, 0.05, 0.05]
+@test_approx_eq padjust([1, 0.5, 0.1, 0.001, 0.0001, 0.002], method = :hommel) [1e+00, 1e+00, 3e-01, 5e-03, 6e-04, 8e-03]
+@test_approx_eq padjust([0.05, 0.05, 0.05], method = :hommel) [0.05, 0.05, 0.05]
 
 pvalues = [0.9954279, 0.9374397, 0.4426119, 0.7564215, 0.1466431, 0.8993471, 0.9507175, 0.1275631, 0.2441196, 0.3745604]
-@test_approx_eq hommel(pvalues) [0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279]
+@test_approx_eq padjust(pvalues, method = :hommel) [0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279, 0.9954279]
 
 pvalues = [0.05, 0.003, 0.51, 0.0001, 0.01, 0.0, 0.008]
-@test_approx_eq hommel(pvalues) [0.1, 0.015, 0.51, 0.0006, 0.03, 0, 0.024]
-@test all(hommel([1,1,1]) .<= 1)
+@test_approx_eq padjust(pvalues, method = :hommel) [0.1, 0.015, 0.51, 0.0006, 0.03, 0, 0.024]
+@test all(padjust([1,1,1], method = :hommel) .<= 1)
 
-@test_throws hommel([-1])
-@test_throws hommel([2])
+@test_throws padjust([-1], method = :hommel)
+@test_throws padjust([2], method = :hommel)
 
-@test hommel([0.5]) == [0.5]
+@test padjust([0.5], method = :hommel) == [0.5]
