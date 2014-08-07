51ed7c5a1f1b701df7e6c7ad352dfb44adcc62fc
Dirk Schumacher <mail@dirk-schumacher.net>
2014-08-07 12:14:15 +0200

    Add macro to validate pvalues

--- src/helper.jl
+++ src/helper.jl
@@ -1,3 +1,9 @@
+macro checkPValues(pValues)
+    return :(if any(($pValues .< 0.0) | ($pValues .> 1.0))
+      throw(DomainError())
+    end)
+end
+
 function order(vector)
   indexes = [1:length(vector)]
   sortedIndexes = sort(indexes, by = (i) -> vector[i])
