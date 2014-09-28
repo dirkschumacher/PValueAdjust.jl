de4889d68562a3ddacbf166ee29467a01d03875f
Dirk Schumacher <mail@dirk-schumacher.net>
2014-09-28 16:15:38 +0200

    Fix a bug

--- test/padjust.jl
+++ test/padjust.jl
@@ -5,4 +5,4 @@ using PValueAdjust
 pValues = [1, 0.5, 0.1, 0.001, 0.0001, 0.002]
 @test_approx_eq padjust(pValues, method = :none) pValues
 @test_approx_eq padjust(pValues) pValues
-@test_throws padjust(pValues, method = :none)
+@test_throws padjust([1], method = :wat)
